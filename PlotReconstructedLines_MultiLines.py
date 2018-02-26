1d083db82b14f45026a072fc3a4c616d3950ca17
Sheu <sheu_ch@dentrassi.af.op.dlr.de>
2018-02-26 17:00:34 +0100

    multiplelines fig updated

--- PlotReconstructedLines_MultiLines.py
+++ PlotReconstructedLines_MultiLines.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import matplotlib.mlab as mlab
 import numpy as np
 
-foldername = '/home/sheu_ch/la/A9/Analysis/';#Test2_img1line1_100/
+foldername = '/home/sheu_ch/la/A9/Analysis/Test4_all/'
 
 Xmax=0
 Ymax=0
@@ -36,13 +36,14 @@ for ii in range(0,703): #,1959)
 		ax.plot(XYZ_yhat[0:,0],XYZ_yhat[0:,1],XYZ_yhat[0:,2],c='r',label='reconstructed lines');
 		#ax.text(XYZ_yhat[-1,0],XYZ_yhat[-1,1],XYZ_yhat[-1,2]+0.2,'%d' % ii,color='b');
 
-		#if XYZ_yhat[-1,2]-XYZ_yhat[0,2]>0.1:
+		#length = np.sqrt((XYZ_yhat[-1,0]-XYZ_yhat[0,0])**2+(XYZ_yhat[-1,1]-XYZ_yhat[0,1])**2)
+		#if length>6:
 		#	ax.plot(XYZ_yhat[0:,0],XYZ_yhat[0:,1],XYZ_yhat[0:,2],c='k',label='reconstructed lines');
-		#	ax.text(XYZ_yhat[-1,0],XYZ_yhat[-1,1],XYZ_yhat[-1,2]+0.2,'%d' % ii,color='k');
+		#	ax.text(XYZ_yhat[0,0],XYZ_yhat[0,1],XYZ_yhat[0,2]+0.2,'%d' % ii,color='k');
 
 
 		### projected; before optimization
-		#ax.plot(XYZ_y[0:,0],XYZ_y[0:,1],XYZ_y[0:,2],c='k',label='DSM profile');
+		ax.plot(XYZ_y[0:,0],XYZ_y[0:,1],XYZ_y[0:,2],c='k',label='DSM profile');
 
 		if XYZ_yhat[0:,0].max()	> Xmax:
 			Xmax = XYZ_yhat[0:,0].max()
@@ -63,28 +64,37 @@ for ii in range(0,703): #,1959)
 		print 'file dist_opti_proj_'+str(ii)+'.txt not found'
 
 
-#### axis.equal ### 
-## Create cubic bounding box to simulate equal aspect ratio
-#max_range = np.array([Xmax-Xmin,Ymax-Ymin,Zmax-Zmin]).max()
-#Xb = 0.5*max_range*np.mgrid[-1:2:2,-1:2:2,-1:2:2][0].flatten() + 0.5*(Xmax+Xmin)
-#Yb = 0.5*max_range*np.mgrid[-1:2:2,-1:2:2,-1:2:2][1].flatten() + 0.5*(Ymax+Ymin)
-#Zb = 0.5*max_range*np.mgrid[-1:2:2,-1:2:2,-1:2:2][2].flatten() + 0.5*(Zmax+Zmin)
-## Comment or uncomment following both lines to test the fake bounding box:
-#for xb, yb, zb in zip(Xb, Yb, Zb):
-#    ax.plot([xb], [yb], [zb], 'w')
-##ax.set_xlim([691200.568959, 691205.331189])
-##ax.set_ylim([5383692.144951, 5383709.486125])
+### axis.equal ### 
+# Create cubic bounding box to simulate equal aspect ratio
+max_range = np.array([Xmax-Xmin,Ymax-Ymin,Zmax-Zmin]).max()
+Xb = 0.5*max_range*np.mgrid[-1:2:2,-1:2:2,-1:2:2][0].flatten() + 0.5*(Xmax+Xmin)
+Yb = 0.5*max_range*np.mgrid[-1:2:2,-1:2:2,-1:2:2][1].flatten() + 0.5*(Ymax+Ymin)
+Zb = 0.5*max_range*np.mgrid[-1:2:2,-1:2:2,-1:2:2][2].flatten() + 0.5*(Zmax+Zmin)
+# Comment or uncomment following both lines to test the fake bounding box:
+for xb, yb, zb in zip(Xb, Yb, Zb):
+	ax.plot([xb], [yb], [zb], 'w')
+#ax.set_xlim([690900.0, 691150.0])
+#ax.set_ylim([5383850.0, 5384000.0])
 
 
 plt.figure(1) 
 ax.set_title('Reconstructed line segments')#, fontsize=10)
 ax.set_xlabel("X axis")
 ax.set_ylabel("Y axis")
+#ax.view_init(30, 90)
 #ax.legend(loc='lower left', bbox_to_anchor=(0,-0.3),fontsize=12)
-plt.subplots_adjust(bottom=0.3)
+
 plt.gcf().set_size_inches(12, 8)
+ax.view_init(elev=45, azim=-75)
 plt.savefig((foldername+'Reconstruction-BeforeAfter.png'), bbox_inches="tight", dpi=300)
+
+#for angle in xrange(20,90,2):
+#	ax.view_init(elev=angle, azim=-60)
+for angle in xrange(30,90,2):
+	ax.view_init(elev=angle, azim=-120)
+	plt.draw();
+	plt.pause(.1)
+	#plt.savefig((foldername+'Reconstruction-BeforeAfter_%d.png' % angle), bbox_inches="tight", dpi=300)
+
 plt.show();
-
-
 
